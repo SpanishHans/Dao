a49c2af4512b1590f5e6f0955942c581a0f731cc
juanjomarg <juanj.martinezg96@gmail.com>
2024-03-13 23:27:18 -0400

    initia;l

---
+++ app/app/src/main/java/edu/dao/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package edu.dao.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
