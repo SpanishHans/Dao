9555273d3f1faa80b3509c02096403741b975eff
juanjomarg <juanj.martinezg96@gmail.com>
2024-03-26 16:38:09 -0400

    Layout a Login y registro

---
+++ app/app/src/main/java/edu/app/dao/login_to_app.kt
@@ -0,0 +1,11 @@
+package edu.app.dao
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginToApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_to_app_layout)
+    }
+}
--- app/app/src/main/java/edu/app/dao/welcome_to_app.kt
+++ app/app/src/main/java/edu/app/dao/welcome_to_app.kt
@@ -10,12 +10,22 @@ class WelcomeToApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_to_app_layout)
+
+        val botonLogin = findViewById<Button>(R.id.button_welcome_login)
+
+        botonLogin.setOnClickListener {
+            val intent = Intent(this, LoginToApp::class.java)
+            startActivity(intent)
+        }
+
         val botonRegistro = findViewById<Button>(R.id.button_welcome_register)
 
         botonRegistro.setOnClickListener {
             val intent = Intent(this, RegisterToApp::class.java)
             startActivity(intent)
         }
+
+
 
     }
 }
