b7f35f31f059fed01863c223d833fbf269dbf15f
juanjomarg <juanj.martinezg96@gmail.com>
2024-03-26 13:30:17 -0400

    Se corrigen errores con el launcher

--- app/app/src/main/java/edu/dao/app/MainActivity.kt
+++ app/app/src/main/java/edu/dao/app/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContentView(R.layout.welcome)
     }
+
 }
