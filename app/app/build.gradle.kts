2207161b41538e6571453b401639b5e76ba8fd9d
juanjomarg <juanj.martinezg96@gmail.com>
2024-04-09 12:51:38 -0500

    Fixes raros de menu e inclusion de inconos

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -39,12 +39,16 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
