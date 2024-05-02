a1fdf5b189117dcacc6d0e983fdea4c6f786f2a9
Ludwig <theludway@gmail.com>
2024-05-02 13:30:43 -0500

    Una parte del nombre de Libo, falta agregar los ejemplos usando la palabra, luego lo hago después de hacer algo de software libre

---
+++ app/app/src/main/java/edu/app/dao/fragments/leccion1/ui/theme/Leccion1PalabraLiBo.kt
@@ -0,0 +1,47 @@
+package edu.app.dao.fragments.leccion1.ui.theme
+
+import android.graphics.Typeface
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import edu.app.dao.R
+import edu.app.dao.databinding.FragmentLeccion1PalabraLiBoBinding
+
+class Leccion1PalabraLiBo : Fragment() {
+
+    private lateinit var binding: FragmentLeccion1PalabraLiBoBinding
+    private var liMediaplayer: MediaPlayer? = null
+    private var boMediaPlayer: MediaPlayer? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLeccion1PalabraLiBoBinding.inflate(inflater, container, false)
+        val toolbarText = requireActivity().findViewById<TextView>(R.id.toolbar_title)
+        val flechaDevolverImagen = requireActivity().findViewById<ImageButton>(R.id.flecha_devolver_imagen)
+        toolbarText.text = "你好-生词"
+        toolbarText.textSize = 35F
+        toolbarText.typeface = Typeface.createFromAsset(requireContext().assets, "fonts/ma_shan_zheng.ttf")
+
+        flechaDevolverImagen.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+            val palabrasNuevas = Leccion1PalabrasNuevas()
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, palabrasNuevas)
+                commit()
+            }
+        }
+
+
+
+
+        return binding.root
+    }
+
+}
