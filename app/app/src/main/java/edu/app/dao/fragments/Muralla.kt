9025b8e7b33fcd2c35cb3f6bbae7803abfdf7623
Ludwig <theludway@gmail.com>
2024-05-06 00:49:32 -0500

    Push para que sirva

--- app/app/src/main/java/edu/app/dao/fragments/Muralla.kt
+++ app/app/src/main/java/edu/app/dao/fragments/Muralla.kt
@@ -10,9 +10,9 @@ import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
-import edu.app.dao.GameFragment
 import edu.app.dao.R
 import edu.app.dao.databinding.FragmentMurallaBinding
+import edu.app.dao.ordenar.FragmentoOrdenar
 
 
 class Muralla : Fragment() {
@@ -39,8 +39,8 @@ class Muralla : Fragment() {
 
 
         binding.nivel1.setOnClickListener {
-            val gamemodeOrdenar = GameFragment()
-            fragmentoNav(gamemodeOrdenar)
+            val gamemodeOrdenar1 = FragmentoOrdenar()
+            fragmentoNav(gamemodeOrdenar1)
         }
 
         return binding.root
