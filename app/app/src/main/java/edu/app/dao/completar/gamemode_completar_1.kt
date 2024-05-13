338b420693d525a26b48253b040f2818edcfc77f
Ludwig <theludway@gmail.com>
2024-05-13 14:42:43 -0500

    Corrección del string raro que aparecia por ahí

--- app/app/src/main/java/edu/app/dao/completar/gamemode_completar_1.kt
+++ app/app/src/main/java/edu/app/dao/completar/gamemode_completar_1.kt
@@ -30,24 +30,6 @@ class FragmentoCompletar : Fragment() {
         contextParagraphTextView = binding.textoEjCompletar5Parte2
         submitButton = binding.botonSubmitCompletar
 
-        // Define the paragraph text with the fill-in-the-blank word
-        val paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet velit nec est gravida [vehicula]. Nam id bibendum ipsum. Duis eleifend orci sit amet urna dapibus, vitae pretium urna pulvinar. Sed ultricies tellus eget nibh fringilla, nec consectetur lacus tempor. Phasellus nec malesuada risus."
-
-        // Create a SpannableString for the paragraph with the fill-in-the-blank word as an EditText
-        val spannableString = SpannableString(paragraph)
-        val fillWordStart = paragraph.indexOf("[")
-        val fillWordEnd = paragraph.indexOf("]") + 1
-        val editText = EditText(requireContext())
-        editText.hint = "Fill in the missing word"
-        editText.setBackgroundColor(ContextCompat.getColor(requireContext(), android.R.color.transparent))
-        spannableString.setSpan(editText, fillWordStart, fillWordEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        val callback = object : OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
-        // Set the SpannableString to the TextView
-        contextParagraphTextView.text = spannableString
 
         submitButton.setOnClickListener {
             val userInput = mutableListOf<String>()
